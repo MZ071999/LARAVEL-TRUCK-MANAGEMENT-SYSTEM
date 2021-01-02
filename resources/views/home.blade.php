04c4d8d23c220df0b0737e1a9c8f60b1d3c4dd8f
MufidaZuhrah <zulhendri.mufida@live.iium.edu.my>
2021-01-02 11:30:35 +0700

    truck views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card text-center">
                 <!-- <div class="card-header">{{ __('Dashboard') }}</div> -->
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -16,10 +15,18 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-                <div class="card-header">{{ __('Choose Which to Track: ') }}</div>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card text-center">
+            <div class="card-header">{{ __('Choose Which to Track: ') }}</div>
                 <div class="card-body">
-                    Truck
+                <a href="{{ route('truck.index')}}">Truck</a>
                 </div>
                 <div class="card-body">
                     Package
@@ -31,4 +38,5 @@
         </div>
     </div>
 </div>
+
 @endsection
---
+++ resources/views/truck/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+
+<h3 class="display-5 text-center">Truck Details</h3> 
+
+@endsection
