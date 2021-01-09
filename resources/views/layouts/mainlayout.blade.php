ba383bbc9cfce08b0f6d79588d6bc1aff228f110
Hafiz24498 <73928018+Hafiz24498@users.noreply.github.com>
2021-01-09 10:27:20 +0800

    Add files via upload

---
+++ resources/views/layouts/mainlayout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        
+
+
+        @include('layouts.partials.head')
+    </head>
+    <body>
+           
+        @yield('content')
+
+        @include('layouts.partials.footer')
+    </body>
+</html>
