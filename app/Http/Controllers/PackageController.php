9837b2928a034e16365c049740b100a9cdd64f33
Hafiz24498 <73928018+Hafiz24498@users.noreply.github.com>
2021-01-09 09:55:06 +0800

    Add files via upload

---
+++ app/Http/Controllers/PackageController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Package;
+use Illuminate\Http\Request;
+
+class PackageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $package = Package::all();
+        return view('package.index', compact('package', $package));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('package.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //Validate
+        $request->validate([
+            'packagenumber' => 'required',
+            'trucknumber' => 'required',
+            'destination' => 'required',
+            'operationdate' => 'required'
+        ]);
+        $package = Task::create([
+            'packagenumber' => $request->packagenumber,
+            'trucknumber' => $request->trucknumber,
+            'destination' => $request->destination,
+            'operationdate' => $request->operationdate
+        ]);
+        return redirect('/task/' .$package->package_id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Package  $package
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Package $package)
+    {
+        return view('package.show', compact('package', $package));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Package  $package
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Package $package)
+    {
+        $package = Package::find($package_id);
+        return view('package.edit', compact('package'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Package  $package
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Package $package)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Package  $package
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Package $package)
+    {
+        //
+    }
+}
