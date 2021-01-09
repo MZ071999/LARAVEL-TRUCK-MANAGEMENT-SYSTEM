223e178b2357d25f2223aa1a6c0b3761a70db223
Hafiz24498 <73928018+Hafiz24498@users.noreply.github.com>
2021-01-09 17:42:01 +0800

    Update create.blade.php

--- resources/views/package/create.blade.php
+++ resources/views/package/create.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.mainlayout')
 
 @section('content')
-    <h1>Add New Package</h1>
+   <div class="container"> 
+       <h1 class="mt-5">Add New Package</h1>
     <hr>
     <form action="/package" method="post">
         {{csrf_field()}}
-        <div class="form-group">
-            <label for="title">Package Number</label>
-            <input type="text" class="form-control" id="packagenumber" name="packagenumber">
-        </div>
-        <div class="form-group">
+        <div class="form-group">   
+             <div class="form-group">
             <label for="title">Truck Number</label>
-            <input type="text" class="form-control" id="trucknumber" name="trucknumber">
+            <input type="text" class="form-control" id="truck_number" name="truck_number">
         </div>
+            <label for="title">Package Number</label>
+            <input type="text" class="form-control" id="package_number" name="package_number">
+        </div>
+    
         <div class="form-group">
             <label for="title">Destination</label>
-            <input type="text" class="form-control" id="detination" name="destination">
+            <input type="text" class="form-control" id="destination" name="destination">
         </div>
         <div class="form-group">
             <label for="title">Operation Date</label>
-            <input type="text" class="form-control" id="operationdate" name="operationdate">
+            <input type="date" class="form-control" id="date_of_operation" name="date_of_operation">
         </div>
         @if($errors->any())
             <div class="alert alert-danger">
@@ -32,5 +34,6 @@
         @endif
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+   </div>
 
 @endsection
