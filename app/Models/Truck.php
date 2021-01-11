c1e995c0222904a4a499275dd68b915dfbf4d226
Hafiz24498 <73928018+Hafiz24498@users.noreply.github.com>
2021-01-11 21:56:44 +0800

    Update Truck.php

--- app/Models/Truck.php
+++ app/Models/Truck.php
@@ -10,4 +10,9 @@ class Truck extends Model
     use HasFactory;
     protected $primaryKey = 'truck_id';
     protected $fillable = ['truck_number', 'no_of_items', 'postman_name', 'date_of_operation', 'status'];
+    
+     public function Package()
+    {
+        return $this->hasMany(Package::class, 'truck_number');
+    }
 }
