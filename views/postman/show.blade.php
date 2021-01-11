ef72ffc047aa6ce808245df39a50e83429bec1c1
Ndaahlb <73925948+Ndaahlb@users.noreply.github.com>
2021-01-11 19:03:30 +0800

    Update show.blade.php

--- views/postman/show.blade.php
+++ views/postman/show.blade.php
@@ -11,6 +11,10 @@
                 <strong>Postman number: </strong> {{$postman->postman_number}}<br>
                 <strong>Postman name: </strong> {{$postman->postman_name}}<br>
                 <strong>Operation Date: </strong> {{$postman->date_of_operation}}<br>
+                <div class="btn-group" role="group" aria-label="Basic example" color="Blue">
+                    <a href="{{url("postman/")}}">
+                        <button type="button" class="btn btn-warning">Back to Index</button>
+                    </a>
             </p>
         </div>
 </div>
