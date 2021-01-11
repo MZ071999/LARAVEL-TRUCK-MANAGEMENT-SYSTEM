e4447305d31ef52c102a690bd06dbdbfd8fcd7de
Hafiz24498 <73928018+Hafiz24498@users.noreply.github.com>
2021-01-11 19:51:13 +0800

    Update show.blade.php

--- resources/views/package/show.blade.php
+++ resources/views/package/show.blade.php
@@ -1,39 +1,40 @@
-@extends('layouts.mainlayout')
+@extends('layouts.app')
 @section('content')
 
-<div class="container">
-        <h1 class="mt-5">Show Package {{$package->package_number}}</h1>
-
-        <div class="jumbotron text-center">
-            <p class="pd-5">
-                <strong>Package ID: </strong> {{$package->package_id}}<br>
-                <strong>Truck number: </strong> {{$package->truck_number}}<br>
-                <strong>Package number: </strong> {{$package->package_number}}<br>
-                <strong>Destination: </strong> {{$package->destination}}<br>
-                <strong>Operation Date: </strong> {{$package->date_of_operation}}<br>
-                <div class="btn-group" role="group" aria-label="Basic example" color="Blue">
-                    <a href="{{url("package/")}}">
-                        <button type="button" class="btn btn-warnig">Back to Index</button>
-                    </a>
-            </p>
+<div class="row">
+    <div class="col-sm-8 offset-sm-2">
+        <br />
+        <h3 class="display-5 text-center">Package Details</h3>
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
+        <br /> 
+        @endif
+            <div class="form-group">
+                <label for="truck_number">Truck Number</label>
+                <input type="text" class="form-control" name="truck_number" value="{{ $package->truck_number }}" readonly>
+            </div>
+            <div class="form-group">
+                <label for="no_of_items">Package Number</label>
+                <input type="text" class="form-control" name="no_of_items" value="{{ $package->package_number }}" readonly>
+            </div>
+            <div class="form-group">
+                <label for="postman_name">Destination</label>
+                <input type="text" class="form-control" name="postman_name" value="{{ $package->destination }}" readonly>
+            </div>
+            <div class="form-group">
+                <label for="date_of_operation">Date of Operation</label>
+                <input type="text" class="form-control" name="date_of_operation" value="{{ $package->date_of_operation }}" readonly>
+            </div>
+            </form>
+            <div class="text-center">
+            <a href="{{ route('package.index')}}" class="btn btn-primary">Return</a>&nbsp;&nbsp;    
+            </div>
+    </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 @endsection
